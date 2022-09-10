9569d62354d83f15ee50d9ddae34ca15eacd390c
Eeli Killström <eeli.killstrom@gmail.com>
2022-09-10 13:16:34 +0300

    feat: example data and endpoint

--- index.ts
+++ index.ts
@@ -1,10 +1,24 @@
 import express, { Express, Request, Response } from 'express'
+import cors from 'cors'
+import Book from './interfaces/book.interface'
 
 
 const app: Express = express()
+app.use(cors())
 
-app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World')
+const EXAMPLE_BOOK: Book = {
+    owner: "John Doe",
+    title: "Python Machine Learning: Machine Learning and Deep Learning with Python, scikit-learn, and TensorFlow 2, 3rd Edition",
+    author: "Klabnik, Steve",
+    topic: "Machine Learning",
+    isbn: "9781492032649",
+    location: "Helsinki"
+}
+
+app.get('/example', function (req: Request, res: Response) {
+    console.log(`Got request from ${req.get("origin")}. Yay!`);
+    res.json(EXAMPLE_BOOK)
 })
   
-app.listen(3000)
+app.listen(3001)
+console.log("Server running on port 3001")

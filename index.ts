df6a70ac17581cd8ca3173a21dfbf8fa1da5b5a4
Eeli Killström <eeli.killstrom@gmail.com>
2022-09-12 22:56:51 +0300

    feat: database queries

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import express, { Express, Request, Response } from 'express'
 import cors from 'cors'
+import mysql from "mysql"
 import Book from './interfaces/book.interface'
 
-
-const app: Express = express()
-app.use(cors())
-
+const DATABASE_NAME = 'efilibrarydb'
+const DATABASE_USER = 'root'
+const DATABASE_PASSWORD = 'superSecretPasswordInPlainText'
 const EXAMPLE_BOOK: Book = {
     owner: "John Doe",
     title: "Python Machine Learning: Machine Learning and Deep Learning with Python, scikit-learn, and TensorFlow 2, 3rd Edition",
@@ -15,10 +15,38 @@ const EXAMPLE_BOOK: Book = {
     location: "Helsinki"
 }
 
+const app: Express = express()
+app.use(cors())
+
+const dbConnection = mysql.createConnection({
+    host: 'localhost',
+    user: DATABASE_USER,
+    password: DATABASE_PASSWORD,
+    database: DATABASE_NAME
+})
+
+//------------- Endpoints ------------
 app.get('/example', function (req: Request, res: Response) {
     console.log(`Got request from ${req.get("origin")}. Yay!`);
     res.json(EXAMPLE_BOOK)
 })
+
+app.get('/allbooks', function (req: Request, res: Response) {
+
+    let queryResult = null
+    dbConnection.connect()
+    dbConnection.query('SELECT * FROM book', (err, rows, fields) => {
+        if (err) throw err
+        queryResult = rows
+        console.log(rows)
+    })
+    dbConnection.end()
+    // Because of asynchronicity, this will log first and res.json will respond with queryValue that is null.
+    // TODO: asynchronous response.
+    console.log(queryResult)
+    res.json(queryResult)
+})
   
+
 app.listen(3001)
 console.log("Server running on port 3001")

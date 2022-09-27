253ed1c7891430d82214bc75f6e2b8d80600f338
miikke79 <74132243+miikke79@users.noreply.github.com>
2022-09-27 21:00:59 +0300

    named test better

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,8 +2,8 @@ import { test, describe } from '@jest/globals';
 import request from 'supertest';
 import app from '../src/index';
  
-describe('testing index file', () => {
-  test('empty string should result in zero', async () => {
+describe('testing app get example', () => {
+  test('get call should return 200', async () => {
     request(app)
     .get("/example")
     .expect("Content-Type", /json/)

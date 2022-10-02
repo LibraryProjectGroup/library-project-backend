d47ce52c5ff4b83e6a1c135d1cb433137a1730d2
Eeli Killström <61108072+Eelii@users.noreply.github.com>
2022-10-02 11:10:28 +0300

    fix: create, update and delete not working

--- sql/EfiLibrary.sql
+++ sql/EfiLibrary.sql
@@ -16,10 +16,12 @@
 
 
 -- Dumping database structure for efilibrarydb
+DROP DATABASE IF EXISTS `efilibrarydb`;
 CREATE DATABASE IF NOT EXISTS `efilibrarydb` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `efilibrarydb`;
 
 -- Dumping structure for table efilibrarydb.library_user
+DROP TABLE IF EXISTS `library_user`;
 CREATE TABLE IF NOT EXISTS `library_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
@@ -32,13 +34,13 @@ CREATE TABLE IF NOT EXISTS `library_user` (
 
 -- Dumping data for table efilibrarydb.library_user: ~4 rows (approximately)
 INSERT INTO `library_user` (`id`, `username`, `passw`, `administrator`) VALUES
-	(3, 'hemuli', 'qweqweqwe', 0),
-	(4, 'joonajoo', 'soin5oeran', 1),
-	(5, 'mikkoR', '4egdv3a453', 0),
-	(6, 'Erika', 'h4whs54htrh', 1);
-
+	(1, 'hemuli', 'qweqweqwe', 0),
+	(2, 'joonajoo', 'soin5oeran', 1),
+	(3, 'mikkoR', '4egdv3a453', 0),
+	(4, 'Erika', 'h4whs54htrh', 1);
 
 -- Dumping structure for table efilibrarydb.book
+DROP TABLE IF EXISTS `book`;
 CREATE TABLE IF NOT EXISTS `book` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `library_user` int(11) NOT NULL,
@@ -49,16 +51,17 @@ CREATE TABLE IF NOT EXISTS `book` (
   `location` varchar(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_book_library_user` (`library_user`),
-  CONSTRAINT `FK_book_library_user` FOREIGN KEY (`library_user`) REFERENCES `library_user` (`id`)
+  CONSTRAINT `FK_book_library_user` FOREIGN KEY (`library_user`) REFERENCES `library_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table efilibrarydb.book: ~3 rows (approximately)
 INSERT INTO `book` (`id`, `library_user`, `title`, `author`, `isbn`, `topic`, `location`) VALUES
-	(4, 3, 'JS for Dummies', 'Mikko Mallikas', '123-456-789', 'JS', 'Nevada'),
-	(5, 4, 'Java for Babies', 'John Doe', '123-5223-789', 'Java', 'Florida'),
-	(6, 5, 'Python for Pets', 'S. Bonsai', '123-456-5623', 'JS', 'Hong Kong');
+	(1, 1, 'JS for Dummies', 'Mikko Mallikas', '123-456-789', 'JS', 'Nevada'),
+	(2, 2, 'Java for Babies', 'John Doe', '123-5223-789', 'Java', 'Florida'),
+	(3, 3, 'Python for Pets', 'S. Bonsai', '123-456-5623', 'JS', 'Hong Kong');
 
 -- Dumping structure for table efilibrarydb.borrowing
+DROP TABLE IF EXISTS `borrowing`;
 CREATE TABLE IF NOT EXISTS `borrowing` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `library_user` int(11) NOT NULL,
@@ -69,15 +72,16 @@ CREATE TABLE IF NOT EXISTS `borrowing` (
   PRIMARY KEY (`id`),
   KEY `library_user` (`library_user`),
   KEY `book` (`book`),
-  CONSTRAINT `FK_borrowing_book` FOREIGN KEY (`book`) REFERENCES `book` (`id`),
-  CONSTRAINT `FK_borrowing_library_user` FOREIGN KEY (`library_user`) REFERENCES `library_user` (`id`)
+  CONSTRAINT `FK_borrowing_book` FOREIGN KEY (`book`) REFERENCES `book` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_borrowing_library_user` FOREIGN KEY (`library_user`) REFERENCES `library_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1 COMMENT='data entry that shows which book was borrowed and when and by which library user';
 
 -- Dumping data for table efilibrarydb.borrowing: ~3 rows (approximately)
 INSERT INTO `borrowing` (`id`, `library_user`, `book`, `dueDate`, `borrowDate`, `returned`) VALUES
-	(5, 3, 4, '2022-08-20', '2022-08-13', b'1'),
-	(6, 5, 5, '2022-08-18', '2022-08-11', b'1'),
-	(7, 4, 6, '2022-09-29', '2022-09-22', b'0');
+	(1, 1, 2, '2022-08-20', '2022-08-13', b'1'),
+	(2, 3, 3, '2022-08-18', '2022-08-11', b'1'),
+	(3, 2, 1, '2022-09-29', '2022-09-22', b'0');
+
 
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;

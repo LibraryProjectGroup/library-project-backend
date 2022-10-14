45dc08338962b72cf9856e93a8b19f504a6a88e3
WuX <axel.riska@gmail.com>
2022-10-14 09:58:10 +0200

    build should be in gitignore

--- build/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./index");
-const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000;
-index_1.app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

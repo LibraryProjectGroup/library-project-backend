a5886b6d8706d03ed7d9c57b2c4642e64a8db49c
miikke79 <74132243+miikke79@users.noreply.github.com>
2022-11-25 13:24:28 +0200

    added health check url

---
+++ src/routes/health.ts
@@ -0,0 +1,15 @@
+import { Request, Response, Router, NextFunction } from "express";
+import { querySelectAllBooks } from "../queries/book";
+
+const router = Router();
+
+router.get("", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        await querySelectAllBooks();
+        res.json({ok: true});
+    } catch (err: any) {
+        res.status(500).json({ok: false, error: err.toString()});
+    }
+});
+
+export default router;

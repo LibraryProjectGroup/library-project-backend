19a37de312e4a004d6a89bbb25de7abcdccced0e
Julia Söderlund <julia.soderlund@yandex.ru>
2023-04-28 14:36:01 +0300

    Return error message when failing to fetch user

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -131,7 +131,16 @@ router.post("/login", async (req: Request, res: Response) => {
       message: "Email and password required",
     });
 
-  let user = await querySelectUserByEmail(email);
+  let user;
+  try {
+    user = await querySelectUserByEmail(email);
+  } catch (error) {
+    return res.status(503).json({
+      ok: false,
+      message: "Unable to connect to the database",
+    });
+  }
+
   if (user == null)
     return res.status(400).json({
       ok: false,

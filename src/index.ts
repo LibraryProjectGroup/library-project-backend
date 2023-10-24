87043857a3c3eaebcbfc21aed40a8b2b54609fdf
divrrv <112497215+divrrv@users.noreply.github.com>
2023-10-24 12:14:24 +0300

    Revert "S23 error logging (#146)"
    This reverts commit def542becc48af3bfca5b3472f73876535ab752f.

--- src/index.ts
+++ src/index.ts
@@ -1,115 +1,109 @@
-import "dotenv/config";
-import express, { Express, NextFunction, Request, Response } from "express";
-import cors from "cors";
-import mysql from "mysql2";
-import expressBearerToken from "express-bearer-token";
-import healthRouter from "./routes/health";
-import authRouter from "./routes/auth";
-import bookRouter from "./routes/book";
-import userRouter from "./routes/user";
-import officeRouter from "./routes/office";
-import borrowRouter from "./routes/borrow";
-import book_listRouter from "./routes/book_list";
-import book_list_entryRouter from "./routes/book_list_entry";
-import book_requestRouter from "./routes/book_request";
-import book_reservationRouter from "./routes/book_reservation";
-import callbackRoute from "./routes/auth/oidc/callback";
-import Session from "./interfaces/session.interface";
+import 'dotenv/config'
+import express, { Express, NextFunction, Request, Response } from 'express'
+import cors from 'cors'
+import mysql from 'mysql2'
+import expressBearerToken from 'express-bearer-token'
+import healthRouter from './routes/health'
+import authRouter from './routes/auth'
+import bookRouter from './routes/book'
+import userRouter from './routes/user'
+import officeRouter from './routes/office'
+import borrowRouter from './routes/borrow'
+import book_listRouter from './routes/book_list'
+import book_list_entryRouter from './routes/book_list_entry'
+import book_requestRouter from './routes/book_request'
+import book_reservationRouter from './routes/book_reservation'
+import callbackRoute from './routes/auth/oidc/callback'
+import Session from './interfaces/session.interface'
 import passwordreset, {
   publicRouter as publicPasswordReset,
-} from "./routes/password_reset";
-import { querySelectSessionBySecret } from "./queries/session";
-import User from "./interfaces/user.interface";
-import { querySelectUserBySessionId } from "./queries/user";
-import cookieParser from "cookie-parser";
-import Logger from "./lib/logger";
-import morganMiddleware from "./config/morganMiddleware";
+} from './routes/password_reset'
+import { querySelectSessionBySecret } from './queries/session'
+import User from './interfaces/user.interface'
+import { querySelectUserBySessionId } from './queries/user'
+import cookieParser from 'cookie-parser'
 
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      DATABASE_SERVER: string;
-      DATABASE_NAME: string;
-      DATABASE_USER: string;
-      DATABASE_PASSWORD: string;
-      PORT: string;
+      DATABASE_SERVER: string
+      DATABASE_NAME: string
+      DATABASE_USER: string
+      DATABASE_PASSWORD: string
+      PORT: string
     }
   }
 
   namespace Express {
     interface Request {
-      session: Session;
-      sessionUser: User;
+      session: Session
+      sessionUser: User
     }
   }
 }
 
 // This is not really considered good practice, but it is an easy fix
 // https://nodejs.org/api/process.html#process_warning_using_uncaughtexception_correctly
-process.on("uncaughtException", (err, origin) => {
-  const errorMessage = `[UNCAUGHT EXCEPTION] at ${new Date().toISOString()}:\n${err}\nUncaught exception origin:\n${origin}`;
-  Logger.error(errorMessage);
-});
+process.on('uncaughtException', (err, origin) => {
+  console.log(
+    `[UNCAUGHT EXCEPTION] at ${new Date().toISOString()}:\n`,
+    err,
+    '\nUncaught exception origin:\n',
+    origin
+  )
+})
 
-const app: Express = express();
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({ credentials: true, origin: true }));
-app.use(cors({ credentials: true, origin: "*" }));
-app.use(expressBearerToken());
-app.use(morganMiddleware);
+const app: Express = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({ credentials: true, origin: true }))
+app.use(cors({ credentials: true, origin: '*' }))
+app.use(expressBearerToken())
 
-app.use("/health", healthRouter);
-app.use("/auth/oidc", callbackRoute);
-app.use("/auth", authRouter);
-app.use("/passwordreset", publicPasswordReset);
+app.use('/health', healthRouter)
+
+app.use('/auth/oidc', callbackRoute)
+app.use('/auth', authRouter)
+app.use('/passwordreset', publicPasswordReset)
 app.use(async (req: Request, res: Response, next: NextFunction) => {
-  if (!req.token) return res.sendStatus(401);
+  if (!req.token) return res.sendStatus(401)
   try {
-    let session = await querySelectSessionBySecret(req.token);
-    if (session == null) return res.sendStatus(401);
-    req.session = session;
-    let user = await querySelectUserBySessionId(session.id);
-    if (user == null) return res.sendStatus(401);
-    req.sessionUser = user;
+    let session = await querySelectSessionBySecret(req.token)
+    if (session == null) return res.sendStatus(401)
+    req.session = session
+    let user = await querySelectUserBySessionId(session.id)
+    if (user == null) return res.sendStatus(401)
+    req.sessionUser = user
 
-    next();
-    return;
+    next()
+    return
   } catch (err) {
-    if (err instanceof Error) {
-      // Log the error message and stack trace
-      Logger.error(err.message); // Log the error message
-      Logger.error(err.stack); // Log the stack trace
-    } else {
-      // Log a generic error message if 'error' is not an instance of Error
-      Logger.error("An error occurred:", err);
-    }
+    console.error(err)
   }
-  res.sendStatus(500);
-});
-
-app.use("/book", bookRouter);
-app.use("/office", officeRouter);
-app.use("/user", userRouter);
-app.use("/borrow", borrowRouter);
-app.use("/booklist", book_listRouter);
-app.use("/booklistentry", book_list_entryRouter);
-app.use("/bookrequest", book_requestRouter);
-app.use("/bookreservation", book_reservationRouter);
-app.use("/passwordreset", passwordreset);
+  res.sendStatus(500)
+})
+app.use('/book', bookRouter)
+app.use('/office', officeRouter)
+app.use('/user', userRouter)
+app.use('/borrow', borrowRouter)
+app.use('/booklist', book_listRouter)
+app.use('/booklistentry', book_list_entryRouter)
+app.use('/bookrequest', book_requestRouter)
+app.use('/bookreservation', book_reservationRouter)
+app.use('/passwordreset', passwordreset)
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  Logger.error(err.message);
+  console.error(err)
   res.status(500).send({
     ok: false,
-  });
-});
+  })
+})
 
 const pool = mysql.createPool({
   host: process.env.DATABASE_SERVER,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-});
+})
 
-export { app, pool };
+export { app, pool }
--- src/queries/__mocks__/borrow.ts
+++ src/queries/__mocks__/borrow.ts
@@ -1,11 +1,11 @@
-import Borrow from "../../interfaces/borrow.interface";
-import DetailedExpiredBorrow from "../../interfaces/detailedExpiredBorrows.interface";
+import Borrow from '../../interfaces/borrow.interface'
+import DetailedExpiredBorrow from '../../interfaces/detailedExpiredBorrows.interface'
 
 // note: this mock is very basic and its purpose is for jest to run through routes
 
-let date = new Date();
-const tenDaysAhead = date.setDate(date.getDate() + 10);
-const dayBehind = date.setDate(date.getDate() - 1);
+let date = new Date()
+const tenDaysAhead = date.setDate(date.getDate() + 10)
+const dayBehind = date.setDate(date.getDate() - 1)
 
 const borrow1: Borrow = {
   id: 1,
@@ -15,7 +15,7 @@ const borrow1: Borrow = {
   dueDate: new Date(tenDaysAhead),
   returnDate: null,
   returned: false,
-};
+}
 
 const borrow2: Borrow = {
   id: 2,
@@ -25,17 +25,17 @@ const borrow2: Borrow = {
   dueDate: new Date(tenDaysAhead),
   returnDate: null,
   returned: false,
-};
+}
 
 const borrow3: Borrow = {
   id: 3,
   library_user: 2,
   book: 2,
-  borrowDate: new Date("2020-01-01"),
-  dueDate: new Date("2020-01-11"),
+  borrowDate: new Date('2020-01-01'),
+  dueDate: new Date('2020-01-11'),
   returnDate: null,
   returned: true,
-};
+}
 
 const borrow4: Borrow = {
   id: 4,
@@ -45,82 +45,82 @@ const borrow4: Borrow = {
   dueDate: new Date(dayBehind),
   returnDate: null,
   returned: false,
-};
+}
 
 const detailedExpiredBorrow1: DetailedExpiredBorrow = {
   borrowId: 4,
-  title: "bt4",
+  title: 'bt4',
   dueDate: borrow4.dueDate,
   bookId: 4,
-  library_user: "t2",
+  library_user: 't2',
   userId: borrow4.library_user,
-};
+}
 
-let mockBorrowData = [borrow1, borrow2, borrow3, borrow4];
-let mockDetailedExpiredBorrowData = [detailedExpiredBorrow1];
-let idCounter = 4;
+let mockBorrowData = [borrow1, borrow2, borrow3, borrow4]
+let mockDetailedExpiredBorrowData = [detailedExpiredBorrow1]
+let idCounter = 4
 
 const getBorrow = (id: number) => {
   for (let index = 0; index < mockBorrowData.length; index++) {
     if (mockBorrowData[index].id === id) {
-      return mockBorrowData[index];
+      return mockBorrowData[index]
     }
   }
-  return null;
-};
+  return null
+}
 
 export const querySelectAllBorrows = async (): Promise<Borrow[]> => {
-  return mockBorrowData as Array<Borrow>;
-};
+  return mockBorrowData as Array<Borrow>
+}
 
 export const querySelectAllCurrentBorrows = async (): Promise<Borrow[]> => {
-  let array: Array<Borrow> = [];
+  let array: Array<Borrow> = []
   mockBorrowData.forEach((element) => {
     if (!element.returned) {
-      array.push(element);
+      array.push(element)
     }
-  });
-  return array as Array<Borrow>;
-};
+  })
+  return array as Array<Borrow>
+}
 
 export const querySelectAllCurrentBorrows2 = async () => {
   let array: {
-    username: string;
-    title: string;
-    borrowDate: Date;
-    dueDate: Date;
-    id: number;
-  }[] = [];
+    username: string
+    title: string
+    borrowDate: Date
+    dueDate: Date
+    id: number
+  }[] = []
   mockBorrowData.forEach((element) => {
     if (!element.returned) {
       array.push({
-        username: "asd",
-        title: "asd",
+        username: 'asd',
+        title: 'asd',
         borrowDate: element.borrowDate,
         dueDate: element.dueDate,
         id: element.book,
-      });
+      })
     }
-  });
-  return array;
-};
+  })
+  return array
+}
 
 export const querySelectBorrow = async (
   borrowingId: number
 ): Promise<Borrow | null> => {
-  return getBorrow(borrowingId);
-};
+  return getBorrow(borrowingId)
+}
 
 export const queryDeleteBorrow = async (
   borrowingId: number
 ): Promise<boolean> => {
-  let deleted = false;
+  let deleted = false
   mockBorrowData = mockBorrowData.filter((borrow) => {
-    if (borrow.id == borrowingId) deleted = true;
-    return borrow.id != borrowingId;
-  });
-  return deleted;
-};
+    if (borrow.id == borrowingId) deleted = true
+    return borrow.id != borrowingId
+  })
+  return deleted
+}
 
 export const queryInsertBorrow = async (
   userId: number,
@@ -136,60 +136,60 @@ export const queryInsertBorrow = async (
     dueDate,
     returned: false,
     returnDate: null,
-  });
-  return true;
-};
+  })
+  return true
+}
 
 export const queryUpdateBorrow = async (borrow: Borrow): Promise<boolean> => {
-  const editedBorrow = getBorrow(borrow.id);
+  const editedBorrow = getBorrow(borrow.id)
   if (editedBorrow) {
-    editedBorrow.book = borrow.book;
-    editedBorrow.borrowDate = borrow.borrowDate;
-    editedBorrow.dueDate = borrow.dueDate;
-    editedBorrow.returned = borrow.returned;
-    return true;
+    editedBorrow.book = borrow.book
+    editedBorrow.borrowDate = borrow.borrowDate
+    editedBorrow.dueDate = borrow.dueDate
+    editedBorrow.returned = borrow.returned
+    return true
   }
-  return false;
-};
+  return false
+}
 
 export const queryBookIsAvailable = async (
   bookId: number
 ): Promise<boolean> => {
-  const borrow = getBorrow(bookId);
-  let availCheck = 0;
+  const borrow = getBorrow(bookId)
+  let availCheck = 0
   if (borrow) {
     mockBorrowData.forEach((element) => {
-      if (!element.returned && element.id == borrow.id) availCheck++;
-    });
+      if (!element.returned && element.id == borrow.id) availCheck++
+    })
   }
-  return availCheck == 0;
-};
+  return availCheck == 0
+}
 
 export const queryBorrowsByUserId = async (
   userId: number
 ): Promise<Borrow[]> => {
-  let array: Array<Borrow> = [];
-  const borrow = getBorrow(userId);
+  let array: Array<Borrow> = []
+  const borrow = getBorrow(userId)
   if (borrow) {
     mockBorrowData.forEach((element) => {
-      if (!element.returned && element.id == borrow.id) array.push(element);
-    });
+      if (!element.returned && element.id == borrow.id) array.push(element)
+    })
   }
-  return array as Array<Borrow>;
-};
+  return array as Array<Borrow>
+}
 
 export const queryExpiredBorrows = async (): Promise<Borrow[]> => {
-  let array: Array<Borrow> = [];
+  let array: Array<Borrow> = []
   mockBorrowData.forEach((element) => {
     if (!element.returned && new Date() > element.dueDate) {
-      array.push(element);
+      array.push(element)
     }
-  });
-  return array as Array<Borrow>;
-};
+  })
+  return array as Array<Borrow>
+}
 
 export const queryDetailedExpiredBorrows = async (): Promise<
   DetailedExpiredBorrow[]
 > => {
-  return mockDetailedExpiredBorrowData as Array<DetailedExpiredBorrow>;
-};
+  return mockDetailedExpiredBorrowData as Array<DetailedExpiredBorrow>
+}

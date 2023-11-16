4343fd5a104657062b1ab6c043e3635347e79d14
Cisse23 <45875060+Cisse23@users.noreply.github.com>
2023-11-16 15:53:34 +0200

    Added fields description and language to book (#174)

--- src/queries/book.ts
+++ src/queries/book.ts
@@ -73,12 +73,27 @@ export const insertNewBook = async (
   year: number,
   isbn: string,
   topic: string,
+  description: string,
+  language: string,
   homeOfficeId: string
 ): Promise<boolean> => {
   const promisePool = pool.promise()
   const [rows] = await promisePool.query<ResultSetHeader>(
-    'INSERT INTO book (library_user, title, image, author, year, topic, isbn, home_office_id) VALUES (?)',
-    [[userId, title, image, author, year, topic, isbn, homeOfficeId]]
+    'INSERT INTO book (library_user, title, image, author, year, topic, isbn, description, language, home_office_id) VALUES (?)',
+    [
+      [
+        userId,
+        title,
+        image,
+        author,
+        year,
+        topic,
+        isbn,
+        description,
+        language,
+        homeOfficeId,
+      ],
+    ]
   )
   return rows.affectedRows != 0
 }
@@ -86,7 +101,7 @@ export const insertNewBook = async (
 export const updateBook = async (book: Book): Promise<boolean> => {
   const promisePool = pool.promise()
   const [rows] = await promisePool.query<ResultSetHeader>(
-    'UPDATE book SET title=(?), image=(?), author=(?), year=(?), topic=(?), isbn=(?), home_office_id=(?) WHERE id=(?)',
+    'UPDATE book SET title=(?), image=(?), author=(?), year=(?), topic=(?), isbn=(?), description=(?), language=(?), home_office_id=(?) WHERE id=(?)',
     [
       book.title,
       book.image,
@@ -94,6 +109,8 @@ export const updateBook = async (book: Book): Promise<boolean> => {
       book.year,
       book.topic,
       book.isbn,
+      book.description,
+      book.language,
       book.homeOfficeId,
       book.id,
     ]
@@ -104,7 +121,7 @@ export const updateBook = async (book: Book): Promise<boolean> => {
 export const getAllReservedBooks = async (): Promise<Book[]> => {
   const promisePool = pool.promise()
   const [rows] = await promisePool.query(
-    'select book.id, book.library_user, book.title, book.image, book.author, book.year, book.isbn, book.topic, book.deleted, ho.home_office_id AS homeOfficeId, ho.name AS homeOfficeName, ho.country_code AS homeOfficeCountry from book JOIN book_reservation AS reservation ON reservation.bookId = book.id JOIN home_office ho USING (home_office_id) WHERE reservation.canceled = 0 AND reservation.loaned = 0 AND book.deleted != 1;'
+    'select book.id, book.library_user, book.title, book.image, book.author, book.year, book.isbn, book.topic, book.description, book.language, book.deleted, ho.home_office_id AS homeOfficeId, ho.name AS homeOfficeName, ho.country_code AS homeOfficeCountry from book JOIN book_reservation AS reservation ON reservation.bookId = book.id JOIN home_office ho USING (home_office_id) WHERE reservation.canceled = 0 AND reservation.loaned = 0 AND book.deleted != 1;'
   )
   return rows as Book[]
 }
